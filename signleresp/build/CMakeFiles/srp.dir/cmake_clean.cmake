68f212cb0f6760acd59312110291511fb046b516
Len Hsu <miniair@hotmail.com>
2022-04-04 10:17:36 -0700

    - Initial commit

---
+++ signleresp/build/CMakeFiles/srp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/srp.dir/main.cpp.o"
+  "srp.pdb"
+  "srp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/srp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
