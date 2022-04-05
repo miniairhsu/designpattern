582d6e78cdf640c755609113c9b4fb0c2728978d
Len Hsu <miniair@hotmail.com>
2022-04-05 09:24:14 -0700

    -Add factory design pattern

---
+++ builderfacets/build/CMakeFiles/srp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/srp.dir/Facets.cpp.o"
+  "CMakeFiles/srp.dir/Person.cpp.o"
+  "CMakeFiles/srp.dir/PersonBuilder.cpp.o"
+  "srp.pdb"
+  "srp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/srp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
